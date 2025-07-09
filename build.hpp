369009c61e41effc49c3e4673e63ab68b869623d
Julien QUENTIN <jqntn88@gmail.com>
2025-07-09 02:12:08 +0200

    log

--- build.hpp
+++ build.hpp
@@ -5,11 +5,35 @@
 #include <cstdio>
 #include <cstdlib>
 #include <filesystem>
+#include <iostream>
 #include <string>
 #include <vector>
 #pragma warning(pop)
 
 namespace fs = std::filesystem;
+
+enum log_severity : char
+{
+  info = 'I',
+  warning = 'W',
+  error = 'E',
+  fatal = 'F',
+};
+
+void
+log(log_severity severity, const std::string& str)
+{
+  FILE* stream = stdout;
+  if (severity == error || severity == fatal) {
+    stream = stderr;
+  }
+
+  fprintf(stream, "%c: %s", severity, str.c_str());
+
+  if (severity == fatal) {
+    std::abort();
+  }
+}
 
 enum target_kind
 {
@@ -29,6 +53,12 @@ enum target_version
   cpp26,
 };
 
+enum target_mode
+{
+  debug,
+  release,
+};
+
 class target
 {
 public:
@@ -39,6 +69,7 @@ public:
 
   void set_kind(target_kind kind) { m_kind = kind; }
   void set_version(target_version version) { m_version = version; }
+  void set_mode(target_mode mode) { m_mode = mode; }
   void add_files(const std::vector<fs::path>& files)
   {
     m_files.insert(m_files.end(), files.begin(), files.end());
@@ -47,11 +78,10 @@ public:
   void build()
   {
     if (m_files.empty()) {
-      std::fprintf(stderr, "no source files have been added");
-      std::abort();
+      log(fatal, "no source files have been added");
     }
 
-    fs::path out_dir = "build.cpp.out";
+    fs::path out_dir = "build";
 
     fs::remove_all(out_dir);
     fs::create_directories(out_dir);
@@ -61,5 +91,6 @@ private:
   std::string m_name = "out";
   target_kind m_kind = target_kind::binary;
   target_version m_version = target_version::cpp20;
+  target_mode m_mode = target_mode::debug;
   std::vector<fs::path> m_files;
 };
--- examples/hello/build.cpp
+++ examples/hello/build.cpp
@@ -5,6 +5,7 @@ main()
 {
   target hello("hello");
 
+  hello.set_mode(debug);
   hello.set_kind(binary);
   hello.set_version(cpp20);
   hello.add_files({ "hello.cpp" });
